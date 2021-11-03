90ca023c134818e21916c11aeddd6bc3a0850206
Frederico Santana <fredericosantana@hotmail.com.br>
2021-11-02 22:15:03 -0300

    Formulários com métodos GET e POST e autenticação de usuários.

---
+++ valida_login.php
@@ -0,0 +1,34 @@
+<?php
+//Variável que verifica se a autenticação foi realizda
+$usurio_autentitado = false;
+
+//Usuários do sistema.
+$usuarios_app = [
+    ['email' => 'adm@teste.com.br', 'senha' => '123456'],
+    ['email' => 'user@teste.com.br', 'senha' => 'abcd'],
+];
+/*
+echo '<pre>';
+print_r($usuarios_app);
+echo '</pre>';
+*/
+foreach ($usuarios_app as $user){
+
+    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+        $usurio_autentitado = true;
+    }
+}
+
+if ($usurio_autentitado) {
+    echo 'Usuário autenticado';
+} else {
+    header('Location: index.php?login=erro');
+}
+
+/*
+print_r($_POST);
+echo '<br/>';
+echo $_POST['email'];
+echo '<br/>';
+echo $_POST['senha'];
+*/
